fd3996a62ae0fab7c286cf3f448c0a3e8578ace0
Joseph Bergantine <jbergantine@gmail.com>
2011-06-29 21:57:34 -0600

    replaced tabs with spaces on a few more files

--- views.py
+++ views.py
@@ -6,19 +6,19 @@ from django.template import RequestContext
 from contact.forms import ContactForm
 
 def contact(request):
-	if request.method == 'POST':
-		form = ContactForm(request.POST)
-		if form.is_valid():
-			cd = form.cleaned_data
-			send_mail(
-				cd['subject'],
-				cd['message'],
-				cd.get('email', cd['email']),
-				[i[1] for i in settings.MANAGERS], # send to managers as defined in project's settings.py file
-			)
-			return HttpResponseRedirect('/contact/thanks/')
-	else:
-		form = ContactForm(
-			#initial={'subject': 'Message from the contact form.'} # prepopulates the subject line
-		)
-	return render_to_response('contact/contact_form.html', {'form': form}, context_instance=RequestContext(request))
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            send_mail(
+                cd['subject'],
+                cd['message'],
+                cd.get('email', cd['email']),
+                [i[1] for i in settings.MANAGERS], # send to managers as defined in project's settings.py file
+            )
+            return HttpResponseRedirect('/contact/thanks/')
+    else:
+        form = ContactForm(
+            #initial={'subject': 'Message from the contact form.'} # prepopulates the subject line
+        )
+    return render_to_response('contact/contact_form.html', {'form': form}, context_instance=RequestContext(request))
