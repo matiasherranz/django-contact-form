f8262d53868c77cf497edcbd4bb5f3c79ff4f0f3
Joseph Bergantine <jbergantine@gmail.com>
2012-03-18 13:08:44 -0600

    closer to PEP8

--- django_contact_form/urls.py
+++ django_contact_form/urls.py
@@ -1,7 +1,8 @@
-from django.conf.urls.defaults import *
+from django.conf.urls.defaults import patterns, include, url
 from django_contact_form.views import contact
+
 
 urlpatterns = patterns('',
     url(r'^$', contact, {}, 'contact'),
-    url(r'^thanks/$', 'django.views.generic.simple.direct_to_template', { 'template': 'contact/success.html' }, 'success'),
+    url(r'^thanks/$', 'django.views.generic.simple.direct_to_template', {'template': 'contact/success.html'}, 'success'),
 )
